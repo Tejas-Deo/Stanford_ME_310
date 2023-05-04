9849aa0d87e35dd24feb4cbc34c2c3fc3499ba45
Tejas-Deo <tejasdeo10@gmail.com>
2023-05-04 15:30:06 -0500

    added alexa pi code

--- alexa_rasp_code.py
+++ alexa_rasp_code.py
@@ -12,25 +12,78 @@ logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 STATUSON = ['on','high']
 STATUSOFF = ['off','low']
 
-@ask.launch
-def launch():
-    speech_text = 'Welcome to Raspberry Pi Automation.'
-    return question(speech_text).reprompt(speech_text).simple_card(speech_text)
+startcommandlist = ['start the autonomous system mode', 'turn on the autonomous system mode']
+stopcommandlist = ['stop the autonomous system mode', 'turn off the autonomous system mode']
+chargingdocklist = ['go to the charging dock', 'go to the charging station']
+moveleftlist = ['move to the left', 'move left', 'go left']
+moverightlist = ['move to the right', 'go right', 'move right']
+movestraightlist = ['go straight', 'move straight']
+comebacklist = ['come back', 'come to me']
 
-@ask.intent('GpioIntent', mapping = {'status':'status'})
-def Gpio_Intent(status,room):
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)    
-    GPIO.setup(17,GPIO.OUT)
-    if status in STATUSON:
-        GPIO.output(17,GPIO.HIGH)
-        return statement('turning {} lights'.format(status))
-    elif status in STATUSOFF:
-        GPIO.output(17,GPIO.LOW)
-        return statement('turning {} lights'.format(status))
-    else:
-        return statement('Sorry not possible.')
- 
+# @ask.launch
+# def launch():
+#     speech_text = 'Welcome to Raspberry Pi Automation.'
+#     return question(speech_text).reprompt(speech_text).simple_card(speech_text)
+
+
+@ask.intent('StartAutonomousSystemIntent', mapping = {'STARTCOMMAND': 'STARTCOMMAND'})
+def StartAutonomousSystemIntent(STARTCOMMAND):
+    if STARTCOMMAND in startcommandlist:
+        return statement(STARTCOMMAND)
+
+
+@ask.intent('StopAutonomousSystemIntent', mapping = {'STOPCOMMAND': 'STOPCOMMAND'})
+def StartAutonomousSystemIntent(STOPCOMMAND):
+    if STOPCOMMAND in stopcommandlist:
+        return statement(STOPCOMMAND)
+
+
+@ask.intent('ChargingDockIntent', mapping = {'CHARGINGCOMMAND': 'CHARGINGCOMMAND'})
+def StartAutonomousSystemIntent(CHARGINGCOMMAND):
+    if CHARGINGCOMMAND in chargingdocklist:
+        return statement(CHARGINGCOMMAND)
+
+
+@ask.intent('MoveLeftIntent', mapping = {'LEFTCOMMAND': 'LEFTCOMMAND'})
+def StartAutonomousSystemIntent(LEFTCOMMAND):
+    if LEFTCOMMAND in moveleftlist:
+        return statement(LEFTCOMMAND)
+
+
+@ask.intent('MoveRightIntent', mapping = {'RIGHTCOMMAND': 'RIGHTCOMMAND'})
+def StartAutonomousSystemIntent(RIGHTCOMMAND):
+    if RIGHTCOMMAND in moverightlist:
+        return statement(RIGHTCOMMAND)
+
+
+@ask.intent('GoStraightIntent', mapping = {'STRAIGHTCOMMAND': 'STRAIGHTCOMMAND'})
+def StartAutonomousSystemIntent(STRAIGHTCOMMAND):
+    if STRAIGHTCOMMAND in movestraightlist:
+        return statement(STRAIGHTCOMMAND)
+
+
+@ask.intent('ComeBackIntent', mapping = {'COMEBACKCOMMAND': 'COMEBACKCOMMAND'})
+def StartAutonomousSystemIntent(COMEBACKCOMMAND):
+    if COMEBACKCOMMAND in comebacklist:
+        return statement(COMEBACKCOMMAND)
+
+
+
+# @ask.intent('GpioIntent', mapping = {'status':'status'})
+# def Gpio_Intent(status,room):
+#     GPIO.setwarnings(False)
+#     GPIO.setmode(GPIO.BCM)    
+#     GPIO.setup(17,GPIO.OUT)
+#     if status in STATUSON:
+#         GPIO.output(17,GPIO.HIGH)
+#         return statement('turning {} lights'.format(status))
+#     elif status in STATUSOFF:
+#         GPIO.output(17,GPIO.LOW)
+#         return statement('turning {} lights'.format(status))
+#     else:
+#         return statement('Sorry not possible.')
+
+
 @ask.intent('AMAZON.HelpIntent')
 def help():
     speech_text = 'You can say hello to me!'
